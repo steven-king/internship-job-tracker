114175a663ddef246f6f23e8e920cad3cf4c80af
bpbuch <bpbuch@live.unc.edu>
2014-03-24 11:55:11 -0400

    django heroku

--- interactive_app/urls.py
+++ interactive_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     url(r'^$', views.home, name ='interactive_app_home'),
     (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^accounts/', include('allauth.urls')),
-        
+    url(r'^user/$', views.user, name='interactive_app_user'),
+	url(r'^organization/$', views.organization, name='interactive_app_organization'),
+	url(r'^city/$', views.city, name='interactive_app_city')
     )
