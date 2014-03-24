ac0d08ee86a5fbd0e582b15539f622951ab1bdfe
bpbuch <bpbuch@live.unc.edu>
2014-03-24 11:38:06 -0400

    django heroku update

--- interactive_app/admin.py
+++ interactive_app/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from interactive_app.models import UserProfile
-
+from interactive_app.models import UserProfile, User, Organization, City
 
 admin.site.register(UserProfile)
+
+class UserAdmin(admin.ModelAdmin):
+	search_fields = ('name',)
+
+admin.site.register(User, UserAdmin)
+
+class OrganizationAdmin(admin.ModelAdmin):
+	search_fields = ('name',)
+
+admin.site.register(Organization, OrganizationAdmin)
+
+class CityAdmin(admin.ModelAdmin):
+	search_fields = ('city',)
+
+admin.site.register(City, CityAdmin)
