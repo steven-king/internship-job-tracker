ddeb83e465da0eff02378307246df6272371c966
bpbuch <bpbuch@live.unc.edu>
2014-03-24 13:01:14 -0400

    heroku master

--- interactive_app/models.py
+++ interactive_app/models.py
@@ -5,6 +5,42 @@ from allauth.socialaccount.models import SocialAccount
 
 import hashlib
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+    about_me = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return "{}'s profile".format(self.user.username)
+
+    class Meta:
+        db_table = 'user_profile'
+
+    def profile_image_url(self):
+        """
+        Return the URL for the user's Facebook icon if the user is logged in via Facebook,
+        otherwise return the user's Gravatar URL
+        """
+        fb_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='facebook')
+
+        if len(fb_uid):
+            return "http://graph.facebook.com/{}/picture?width=40&height=40".format(fb_uid[0].uid)
+
+        return "http://www.dotnetcodesg.com/Register/UserImage/Default.png".format(
+            hashlib.md5(self.user.email).hexdigest())
+
+    def account_verified(self):
+        """
+        If the user is logged in and has verified hisser email address, return True,
+        otherwise return False
+        """
+        if self.user.is_authenticated:
+            result = EmailAddress.objects.filter(email=self.user.email)
+            if len(result):
+                return result[0].verified
+        return False
+
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 
 class User(models.Model):
     name = models.TextField(null=True)
@@ -43,41 +79,3 @@ class Organization(models.Model):
 class City(models.Model):
     name = models.TextField(null=True)
     state = models.TextField(null=True)
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_name='profile')
-    about_me = models.TextField(null=True, blank=True)
-
-    def __unicode__(self):
-        return "{}'s profile".format(self.user.username)
-
-    class Meta:
-        db_table = 'user_profile'
-
-    def profile_image_url(self):
-        """
-        Return the URL for the user's Facebook icon if the user is logged in via Facebook,
-        otherwise return the user's Gravatar URL
-        """
-        fb_uid = SocialAccount.objects.filter(user_id=self.user.id, provider='facebook')
-
-        if len(fb_uid):
-            return "http://graph.facebook.com/{}/picture?width=40&height=40".format(fb_uid[0].uid)
-
-        return "http://www.dotnetcodesg.com/Register/UserImage/Default.png".format(
-            hashlib.md5(self.user.email).hexdigest())
-
-    def account_verified(self):
-        """
-        If the user is logged in and has verified hisser email address, return True,
-        otherwise return False
-        """
-        if self.user.is_authenticated:
-            result = EmailAddress.objects.filter(email=self.user.email)
-            if len(result):
-                return result[0].verified
-        return False
-
-
-User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
