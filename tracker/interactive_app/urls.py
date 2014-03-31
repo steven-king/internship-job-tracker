b0c3a9be2a42b1b6ef62939254499112be63c85e
anniedaniel <anniedaniel001@gmail.com>
2014-03-31 01:53:18 -0400

    added citylist page, lots of data issues, so many css issues. i don't know what's wrong- will discuss tomorrow

--- tracker/interactive_app/urls.py
+++ tracker/interactive_app/urls.py
@@ -6,7 +6,8 @@ urlpatterns = patterns('',
     url(r'^$', views.home, name ='interactive_app_home'),
     (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^user/$', views.user, name='interactive_app_user'),
+    url(r'^user/$', views.userList, name='interactive_app_user_list'),
+    url(r'^user/(?P<pk>\d+)$', views.user, name='interactive_app_user'),
 	url(r'^organization/$', views.organization, name='interactive_app_organization'),
-	url(r'^city/$', views.city, name='interactive_app_city')
+	url(r'^city/$', views.cityList, name='interactive_app_city_list')
     )
