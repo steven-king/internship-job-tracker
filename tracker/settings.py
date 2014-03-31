c4af822ace4d76cb4d17085873b5743c351181e4
bpbuch <bpbuch@live.unc.edu>
2014-03-31 10:33:18 -0400

    heroku update

--- tracker/settings.py
+++ tracker/settings.py
@@ -195,21 +195,21 @@ LOGGING = {
 
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_COPPER_URL"])
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_COPPER_URL"])
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
+# # Allow all host headers
+# ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static_media/'
+# # Static asset configuration
+# import os
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static_media/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_media'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static_media'),
+# )
